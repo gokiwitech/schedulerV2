22b36214d357b536cb653960b7ee9adccb55ec1d
kunal-kiwi <kunal.chandel@gokiwi.in>
2024-07-23 19:10:56 +0530

    feat(schedulerV2): added goroutine to authenticate the token every 10 minutes

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"flag"
 	"log"
+	"os/exec"
 	"schedulerV2/config"
 	"schedulerV2/middleware"
 	"schedulerV2/models"
 	"schedulerV2/routers"
 	"schedulerV2/services"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -31,6 +33,34 @@ func init() {
 	config.InitZooKeeper(strings.Split(models.AppConfig.ZookeeperHosts, ",")) // list of zookeeper servers
 
 	services.InitServices()
+
+	// Timer to fetch PGPASSWORD token every 10 minutes
+	go func() {
+		for {
+			pgPasswordCmd := exec.Command("aws", "rds", "generate-db-auth-token", "--hostname", models.AppConfig.DatabaseHost, "--port", models.AppConfig.DatabasePort, "--region", models.AppConfig.DatabaseRegion, "--username", models.AppConfig.DatabaseUser)
+			pgPasswordOutput, err := pgPasswordCmd.Output()
+			if err != nil {
+				log.Fatal("Error executing aws command:", err)
+			} else {
+				pgPassword := strings.TrimSpace(string(pgPasswordOutput))
+				// Read existing environment variables from .env file
+				envVars, err := godotenv.Read(".env")
+				if err != nil {
+					log.Println("Error reading .env file:", err)
+				}
+
+				// Update only the PGPASSWORD value
+				envVars["PGPASSWORD"] = pgPassword
+
+				// Write the updated environment variables back to .env file
+				err = godotenv.Write(envVars, ".env")
+				if err != nil {
+					log.Println("Error updating PGPASSWORD in .env file:", err)
+				}
+			}
+			time.Sleep(10 * time.Minute)
+		}
+	}()
 }
 
 func main() {
